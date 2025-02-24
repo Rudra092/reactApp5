720570bad66fea029c72cd5d9ed4ca01d191a39f
Rudra092 <rudra.092@gmail.com>
2025-02-24 16:56:03 +0530

    third commit

--- src/components/header.js
+++ src/components/header.js
@@ -150,6 +150,15 @@ export default function AppHeader(){
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
+              <Link to="/" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                  Home
+                </Link>
+                <Link to="/about-inner" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                  About
+                </Link>
+                <Link to="/services-inner" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                  Services
+                </Link>
                 <Disclosure as="div" className="-mx-3">
                   <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
                     Product
